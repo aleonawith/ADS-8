aad8c28253e6a63d45e2847b34c7838b53adb48c
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-05-04 19:37:44 +0300

    Update train.h
    Смирнова Алена 21-ИСТ-2

--- include/train.h
+++ include/train.h
@@ -9,8 +9,9 @@ class Train {
     Cage *next;
     Cage *prev;
   };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Cage *first; // точка входа в поезд (первый вагон)
+  int countOp, length, Count; // счетчик шагов (число переходов из вагона в вагон)
+  Cage *first, *current; // точка входа в поезд (первый вагон)
+  Train::Cage *create(bool light);
  public:
   Train();
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
